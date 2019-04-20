c62ab37b674808d5ec3080c681fbd06402b310b5
Mike Scherban <mike.scherban@gmail.com>
2019-04-20 02:33:20 +0000

    draw rectangles around our yellow triangles

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 CVFLAGS=`pkg-config --cflags --libs opencv`
 CFLAGS=-Wall -O2
+LIBS=-lrt
 CC=g++
 
 main: main.o
-	$(CC) -o $@ $< $(CFLAGS) $(CVFLAGS)
+	$(CC) -o $@ $< $(CFLAGS) $(LIBS) $(CVFLAGS)
 
 version: version.o
-	$(CC) -o $@ $< $(CFLAGS) $(CVFLAGS)
+	$(CC) -o $@ $< $(CFLAGS) $(LIBS) $(CVFLAGS)
 
 clean: 
 	rm -rf version main *.o
 
 %.o: %.cpp
-	$(CC) -c -o $@ $< $(CFLAGS) $(CVFLAGS)
+	$(CC) -c -o $@ $< $(CFLAGS) $(LIBS) $(CVFLAGS)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
+#include <time.h>
 
 using namespace cv;
 using namespace std;
@@ -13,7 +14,12 @@ using namespace std;
 #define DEF_MIN_V		50
 #define DEF_MAX_V		255
 
+RNG rng(12345);
+
 int main(int argc, char** argv) {
+	struct timespec start, end;
+	uint64_t diff;
+	
 	int minh = DEF_MIN_H, maxh = DEF_MAX_H;
 	int mins = DEF_MIN_S, maxs = DEF_MAX_S;
 	int minv = DEF_MIN_V, maxv = DEF_MAX_V;
@@ -36,6 +42,7 @@ int main(int argc, char** argv) {
 	cout << "sat: " << mins << "-" << maxs << endl;
 	cout << "val: " << minv << "-" << maxv << endl;
 	
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
 	
 	Mat frame, hsv, threshold;
 #if defined(USE_CAMERA)
@@ -51,7 +58,7 @@ int main(int argc, char** argv) {
 	
 	camera >> frame; // capture first frame
 #else
-	frame = imread("yellow_triangles.png");
+	frame = imread("images/yellow_triangles_star_red_rect.png");
 #endif
 
 	
@@ -60,16 +67,55 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	
-	cvtColor(frame, hsv, CV_BGR2HSV);
+	
+	
+	//convert to hue/saturation/value
+	cvtColor(frame, hsv, CV_BGR2HSV); 
+	//yellow pass through, get binary image (black and white)
 	inRange(hsv, Scalar(minh/2,mins,minv), Scalar(maxh/2,maxs,maxv), threshold);
 	
-	vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+	//storage vector for our contours
+	//vector<Point> is one contour, held in another vector
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	//get the contours
+	findContours(threshold, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 	
-	imwrite("capture.png", frame); // save capture
-	imwrite("hsv.png", hsv);
-	imwrite("threshold.png", threshold);
+	//at this point there is a vector of contours, but we just want
+	//2 triangles, make sure we have those.
+	vector<vector<Point>> triangles;
+	for (unsigned int i = 0; i < contours.size(); i++) {
+		vector<Point> poly;
+		approxPolyDP(contours[i], poly, 0.07 * arcLength(contours[i], 1), 1);
+		if (poly.size() == 3) {
+			triangles.push_back(poly);
+		}
+	}
+	
+	if (triangles.size() != 2) {
+		cout << "Error, did not find 2 triangles." << endl;
+	}
+	
+	Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
+	
+	vector<Rect> rectangles(2);
+	for (unsigned int i = 0; i < 2; i++) {
+		rectangles[i] = boundingRect(triangles[i]);
+		
+		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+		rectangle( drawing, rectangles[i].tl(), rectangles[i].br(), color, 2, 8, 0 );
+	}
+	
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &end);
+	imwrite("images/capture.png", frame); // save capture
+	//imwrite("hsv.png", hsv);
+	imwrite("images/threshold.png", threshold);
+	imwrite("images/drawing.png", drawing);
+	
+	diff = 1000000000 * (end.tv_sec - start.tv_sec) +
+			end.tv_nsec - start.tv_nsec;
 	
 	cout << "Done." << endl;
+	cout << "Time: " << diff / 1000000 << " ms" << endl;
 	return 0;
 }
